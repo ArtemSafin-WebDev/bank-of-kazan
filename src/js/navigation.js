9789bbb7895b8d787f292ef6c84c8d111d043a5e
Artem Safin <artemsafin.webdev@gmail.com>
2020-05-12 20:37:04 +0300

    Основной функционал навигационного блока

--- src/js/navigation.js
+++ src/js/navigation.js
@@ -8,9 +8,22 @@ export default function() {
         const bgSliders = Array.from(element.querySelectorAll('.navigation__bg-slider .swiper-container'));
         const innerSliders = Array.from(element.querySelectorAll('.navigation__content-inner-slider'));
         const navigationMenuLayers = Array.from(element.querySelectorAll('.navigation__main-menu-layer'));
+        const toggleCategoryLayer = element.querySelector('.navigation__universal-toggle-select-category');
+        const searchFormLayer = element.querySelector('.navigation__universal-toggle-search-form');
         const categoryToggles = Array.from(element.querySelectorAll('.navigation__universal-toggle-select-category-link'));
         const searchToggleBtn = element.querySelector('.navigation__universal-toggle-search');
         const searchForm = element.querySelector('.navigation__universal-toggle-search-form');
+        const innerSliderLayers = Array.from(element.querySelectorAll('.navigation__content-inner-slider-layer'));
+        const backplate = element.querySelector('.navigation__backplate');
+        const searchClose = element.querySelector('.navigation__universal-search-close');
+        const searchInput = element.querySelector('.navigation__universal-toggle-search-form-input');
+        const allNavigationTabs = Array.from(document.querySelectorAll('.navigation__content-right-col-menu-card'));
+        const allNavLinks = Array.from(element.querySelectorAll('.navigation__main-menu-link'));
+        const innerSlidersLayer = element.querySelector('.navigation__content-right-col-sliders');
+        const innerMenusLayer = element.querySelector('.navigation__content-right-col-menus');
+        const navMenuGroups = Array.from(element.querySelectorAll('.navigation__content-right-col-menus-group'));
+        const menusCloseBtn = element.querySelector('.navigation__menus-close-btn');
+
         let forCompanies = false;
         let searchOpen = false;
         let activeCategoryIndex = 0;
@@ -25,6 +38,7 @@ export default function() {
                     watchOverflow: true,
                     autoHeight: false,
                     effect: 'fade',
+                    allowTouchMove: false,
                     speed: 1000,
                     fadeEffect: {
                         crossFade: true
@@ -35,25 +49,24 @@ export default function() {
                     }
                 });
 
-                console.log(
-                    {
-                        nextEl: sliderWrapper.querySelector('.navigation__content-inner-slider-arrow--next'),
-                        prevEl: sliderWrapper.querySelector('.navigation__content-inner-slider-arrow--prev')
-                    }
-                )
+                console.log({
+                    nextEl: sliderWrapper.querySelector('.navigation__content-inner-slider-arrow--next'),
+                    prevEl: sliderWrapper.querySelector('.navigation__content-inner-slider-arrow--prev')
+                });
 
                 const bgSlider = new Swiper(bgSliderContainer, {
                     slidesPerView: 1,
+                    slidesPerView: 1,
                     effect: 'fade',
+                    speed: 1800,
+                    allowTouchMove: false,
                     fadeEffect: {
-                        crossFade: true
+                        crossFade: false
                     }
                 });
 
-                bgSlider.params.control = innerSlider;
-
-                // innerSlider.params.control = bgSlider;
-               
+                bgSlider.controller.control = innerSlider;
+                innerSlider.controller.control = bgSlider;
             });
         }
 
@@ -61,26 +74,85 @@ export default function() {
             categoryToggles.forEach(btn => btn.classList.remove('active'));
             bgLayers.forEach(layer => layer.classList.remove('active'));
             navigationMenuLayers.forEach(layer => layer.classList.remove('active'));
+            innerSliderLayers.forEach(layer => layer.classList.remove('active'));
             bgLayers[btnIndex].classList.add('active');
             categoryToggles[btnIndex].classList.add('active');
             navigationMenuLayers[btnIndex].classList.add('active');
+            innerSliderLayers[btnIndex].classList.add('active');
 
             activeCategoryIndex = btnIndex;
         }
 
+        function initializeNavigationMenu() {
+            navigationMenuLayers.forEach(layer => {
+                const navLinks = Array.from(layer.querySelectorAll('.navigation__main-menu-link'));
 
+                navLinks.forEach((link, linkIndex) => {
+                    link.addEventListener('click', event => {
+                        event.preventDefault();
+                     
+                        allNavLinks.forEach(link => link.classList.remove('active'));
+                        link.classList.add('active');
+                        showNavigationMenuTab(linkIndex);
+                    });
+                });
+            });
+        }
+
+        function showNavigationMenuTab(index) {
+            backplate.classList.add('open');
+            innerSlidersLayer.classList.remove('active');
+            innerMenusLayer.classList.add('active');
+            menusCloseBtn.classList.add('active');
+            element.classList.add('nav-menu-open');
+          
+            allNavigationTabs.forEach(element => element.classList.remove('active'));
+            const currentMenuGroup = navMenuGroups[activeCategoryIndex];
+            const cardsInGroup = Array.from(currentMenuGroup.querySelectorAll('.navigation__content-right-col-menu-card'));
+
+            cardsInGroup.forEach((card, cardIndex) => {
+                if (cardIndex === index) {
+                    card.classList.add('active');
+                }
+            })
         
+        }
+
+        function closeNavigationMenu() {
+            backplate.classList.remove('open');
+            innerSlidersLayer.classList.add('active');
+            innerMenusLayer.classList.remove('active');
+            menusCloseBtn.classList.remove('active');
+
+           
+            allNavLinks.forEach(link => link.classList.remove('active'));
+            element.classList.remove('nav-menu-open');
+
+            allNavigationTabs.forEach(element => element.classList.remove('active'));
+        }
 
         function handleSearchBtn() {
             if (!searchOpen) {
                 searchOpen = true;
+                toggleCategoryLayer.classList.remove('active');
+                searchFormLayer.classList.add('active');
+                searchClose.classList.add('active');
             } else {
                 searchForm.submit();
             }
         }
 
+        function handleSearchCloseBtn() {
+            searchOpen = false;
+            toggleCategoryLayer.classList.add('active');
+            searchFormLayer.classList.remove('active');
+            searchClose.classList.remove('active');
+            searchInput.value = '';
+        }
+
         handleCategoryClick(activeCategoryIndex);
         initializeInnerSliders();
+        initializeNavigationMenu();
 
         categoryToggles.forEach((btn, btnIndex) =>
             btn.addEventListener('click', event => {
@@ -92,6 +164,16 @@ export default function() {
         searchToggleBtn.addEventListener('click', function(event) {
             event.preventDefault();
             handleSearchBtn();
+        });
+
+        searchClose.addEventListener('click', function(event) {
+            event.preventDefault();
+            handleSearchCloseBtn();
+        });
+
+        menusCloseBtn.addEventListener('click', function(event) {
+            event.preventDefault();
+            closeNavigationMenu();
         });
     });
 }
