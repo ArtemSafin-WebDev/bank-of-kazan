b0ffa61018c9aaa999f67f10935a33e455625ad1
Artem Safin <artemsafin.webdev@gmail.com>
2020-11-11 22:04:20 +0300

    Блок с этапами заявки

--- src/js/polyfills.js
+++ src/js/polyfills.js
@@ -2,10 +2,13 @@ import cssVars from 'css-vars-ponyfill';
 import lazySizes from 'lazysizes';
 import 'lazysizes/plugins/parent-fit/ls.parent-fit';
 import 'lazysizes/plugins/respimg/ls.respimg';
+import smoothscroll from 'smoothscroll-polyfill';
 
 if (!('object-fit' in document.createElement('a').style)) {
     require('lazysizes/plugins/object-fit/ls.object-fit');
 }
+
+smoothscroll.polyfill();
 
 export default function() {
     // Полифилл .contains для IE 11
