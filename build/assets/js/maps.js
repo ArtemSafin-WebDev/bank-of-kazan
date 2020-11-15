fc5d08b64ffea4ea35755a157ed8df70b7e0d6d8
Artem Safin <artemsafin.webdev@gmail.com>
2020-11-15 23:38:44 +0300

    Изменения по карте

--- build/assets/js/maps.js
+++ build/assets/js/maps.js
@@ -1,39 +1,36 @@
 ymaps.ready(init);
 function init() {
-    var mainMap = document.querySelector('.js-main-offices-map');
+    const mainMap = document.querySelector('.js-main-offices-map');
     if (!mainMap) return;
-    var mainMapInstance = new ymaps.Map(mainMap, {
+    const map = new ymaps.Map(mainMap, {
         center: [55.796554, 49.104752],
         zoom: 12,
         controls: []
     });
+    const checkboxes = Array.from(document.querySelectorAll('.js-office-categories .offices__checkbox-input'));
+    const allCheckbox = checkboxes.find(element => element.name === 'all');
+    const innerMapElements = Array.from(document.querySelectorAll('.js-offices-inner-map'));
 
-    mainMapInstance.controls.add('zoomControl');
-
-    if (window.matchMedia('(min-width: 769px)').matches) {
-        mainMapInstance.controls.add('searchControl');
-    }
-
-    var mapPinImages = {
-        office: {
+    const mapPinImages = {
+        offices: {
             iconLayout: 'default#image',
             iconImageHref: 'img/map-pins/office.svg',
             iconImageSize: window.matchMedia('(min-width: 769px)').matches ? [25, 37] : [25, 37],
             iconImageOffset: window.matchMedia('(min-width: 769px)').matches ? [-12.5, -37] : [-25, -74]
         },
-        bankomat: {
+        bankomats: {
             iconLayout: 'default#image',
             iconImageHref: 'img/map-pins/bankomat.svg',
             iconImageSize: window.matchMedia('(min-width: 769px)').matches ? [25, 37] : [25, 37],
             iconImageOffset: window.matchMedia('(min-width: 769px)').matches ? [-12.5, -37] : [-25, -74]
         },
-        bankomatPartners: {
+        partners: {
             iconLayout: 'default#image',
             iconImageHref: 'img/map-pins/bankomat-partner.svg',
             iconImageSize: window.matchMedia('(min-width: 769px)').matches ? [25, 37] : [25, 37],
             iconImageOffset: window.matchMedia('(min-width: 769px)').matches ? [-12.5, -37] : [-25, -74]
         },
-        terminal: {
+        terminals: {
             iconLayout: 'default#image',
             iconImageHref: 'img/map-pins/terminal.svg',
             iconImageSize: window.matchMedia('(min-width: 769px)').matches ? [25, 37] : [25, 37],
@@ -41,176 +38,171 @@ function init() {
         }
     };
 
-    var mainMapData = [
+    const mainMapData = [
         {
-            type: 'office',
+            type: 'offices',
             coords: [55.831082, 49.079644]
         },
         {
-            type: 'office',
+            type: 'offices',
             coords: [55.815159, 49.101276]
         },
         {
-            type: 'office',
+            type: 'offices',
             coords: [55.812957, 49.183735]
         },
         {
-            type: 'office',
+            type: 'offices',
             coords: [55.79474, 49.114071]
         },
         {
-            type: 'bankomat',
+            type: 'bankomats',
             coords: [55.833308, 49.132141]
         },
         {
-            type: 'bankomat',
+            type: 'bankomats',
             coords: [55.776266, 49.140724]
         },
         {
-            type: 'bankomat-partner',
+            type: 'partners',
             coords: [55.748973, 49.104675]
         },
         {
-            type: 'terminal',
+            type: 'terminals',
             coords: [55.744519, 49.218315]
         }
     ];
 
-    var allCheckbox = document.querySelector('#all-offices');
-    var officesCheckbox = document.querySelector('#offices');
-    var bankomatsCheckbox = document.querySelector('#bankomats');
-    var bankomatsPartnersCheckbox = document.querySelector('#partners-bankomats');
-    var terminalsCheckbox = document.querySelector('#terminals');
-
-    var checkboxes = [allCheckbox, officesCheckbox, bankomatsCheckbox, bankomatsPartnersCheckbox, terminalsCheckbox];
-
-    var placemarksGeoQuery = null;
-
-    function setMarkers() {
-        if (placemarksGeoQuery) placemarksGeoQuery.removeFromMap(mainMapInstance);
-        placemarksGeoQuery = null;
-        var placemarks = [];
-        var filteredObjects = [];
-
-        filteredObjects = mainMapData.filter(function(item) {
-            if (allCheckbox.checked) return true;
-
-            if (item.type === 'office' && officesCheckbox.checked) {
-                return true;
-            } else if (item.type === 'bankomat' && bankomatsCheckbox.checked) {
-                return true;
-            } else if (item.type === 'bankomat-partner' && bankomatsPartnersCheckbox.checked) {
-                return true;
-            } else if (item.type === 'terminal' && terminalsCheckbox.checked) {
-                return true;
-            } else {
-                return false;
-            }
-        });
-
-        console.log('Filtered map objects', filteredObjects);
-
-        filteredObjects.forEach(item => {
-            var pinOptions = null;
-            if (item.type === 'office') {
-                pinOptions = mapPinImages.office;
-            } else if (item.type === 'bankomat') {
-                pinOptions = mapPinImages.bankomat;
-            } else if (item.type === 'bankomat-partner') {
-                pinOptions = mapPinImages.bankomatPartners;
-            } else if (item.type === 'terminal') {
-                pinOptions = mapPinImages.terminal;
-            }
-            var placemark = new ymaps.Placemark(item.coords, {}, pinOptions);
-            placemarks.push(placemark);
-
-            mainMapInstance.geoObjects.add(placemark);
-        });
-
-        placemarksGeoQuery = ymaps.geoQuery(placemarks);
-    }
-
-    if (allCheckbox.checked) {
-        checkboxes.forEach(checkbox => {
-            checkbox.checked = false;
-            allCheckbox.checked = true;
-        });
-    }
-
-    const listViewItems = Array.from(document.querySelectorAll('.offices__list-view-group'));
-
-    function filterListViewItems() {
-        const hiddenItems = listViewItems.filter(element => {
-            if (element.getAttribute('data-type') === 'office' && !officesCheckbox.checked) {
-                return true;
-            }
-            if (element.getAttribute('data-type') === 'bankomat' && !bankomatsCheckbox.checked) {
-                return true;
-            }
-            if (element.getAttribute('data-type') === 'bankomat-partner' && !bankomatsPartnersCheckbox.checked) {
-                return true;
-            }
-            if (element.getAttribute('data-type') === 'terminal' && !terminalsCheckbox.checked) {
-                return true;
-            }
-
-            return false;
-        });
-
-
-        console.log('Filtering hidden items', hiddenItems)
-
-        listViewItems.forEach(item => item.classList.remove('hidden'));
-
-        hiddenItems.forEach(item => item.classList.add('hidden'));
-    }
-
-    setMarkers();
-
-    filterListViewItems();
-
-    checkboxes.forEach(function(checkbox) {
-        checkbox.addEventListener('change', () => {
-            if (checkbox === allCheckbox && allCheckbox.checked) {
-                console.log('Clicking on all checkbox');
-                checkboxes.forEach(box => (box.checked = false));
-                allCheckbox.checked = true;
-            } else {
-                allCheckbox.checked = false;
-            }
-
-            setMarkers();
-
-            filterListViewItems();
-        });
+    map.controls.add('zoomControl', {
+        position: {
+            left: 'auto',
+            right: 20,
+            bottom: 40,
+            top: 'auto'
+        }
     });
 
-    var innerMaps = Array.prototype.slice.call(document.querySelectorAll('.js-offices-inner-map'));
+    const objectManager = new ymaps.ObjectManager({
+        clusterize: false,
+        gridSize: 128,
+        clusterIconLayout: 'default#pieChart',
+        clusterHasBalloon: false
+    });
 
-    innerMaps.forEach(function(mapElement) {
-        var lat = mapElement.getAttribute('data-lat');
-        var lng = mapElement.getAttribute('data-lng');
-        var type = mapElement.getAttribute('data-type');
-        var innerMapInstance = new ymaps.Map(mapElement, {
+    map.geoObjects.add(objectManager);
+
+    const filterObjects = () => {
+        if (allCheckbox.checked) {
+            const filterString = '';
+            objectManager.setFilter(filterString);
+        } else {
+            const activeCheckboxes = [];
+
+            checkboxes.map(checkbox => {
+                if (checkbox.checked) {
+                    activeCheckboxes.push(`properties.type == "${checkbox.name}"`);
+                }
+            });
+
+            const filterString = activeCheckboxes.length ? activeCheckboxes.join(' || ') : "properties.type == 'none'";
+
+            objectManager.setFilter(filterString);
+        }
+    };
+
+    const filterListViewItems = () => {
+        const items = Array.from(document.querySelectorAll('.offices__list-view-group'));
+        if (allCheckbox.checked) {
+            items.forEach(item => item.classList.remove('hidden'));
+        } else {
+            const activeCheckboxesNames = checkboxes.filter(checkbox => checkbox.checked).map(checkbox => checkbox.name);
+
+            items.forEach(item => {
+                const type = item.getAttribute('data-type');
+
+                if (activeCheckboxesNames.includes(type)) {
+                    item.classList.remove('hidden');
+                } else {
+                    item.classList.add('hidden');
+                }
+            });
+
+            console.log('Active checkboxes for list view', activeCheckboxesNames);
+        }
+    };
+
+    filterObjects();
+    filterListViewItems();
+
+    checkboxes.forEach(element =>
+        element.addEventListener('change', () => {
+            filterObjects();
+            filterListViewItems();
+        })
+    );
+
+    mainMapData.forEach(item => {
+        const objectToAdd = {
+            id: item.coords.join('-'),
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: item.coords
+            },
+            options: {
+                ...mapPinImages[item.type]
+            },
+            properties: {
+                type: item.type
+            }
+        };
+        // console.log('Object to add', objectToAdd);
+
+        objectManager.add(objectToAdd);
+    });
+
+    innerMapElements.forEach(mapElement => {
+        const lat = mapElement.getAttribute('data-lat');
+        const lng = mapElement.getAttribute('data-lng');
+        const type = mapElement.getAttribute('data-type');
+
+        const innerMapInstance = new ymaps.Map(mapElement, {
             center: [lat, lng],
             zoom: 12,
             controls: []
         });
 
-        innerMapInstance.controls.add('zoomControl');
-
-        var pinOptions = null;
-        if (type === 'office') {
-            pinOptions = mapPinImages.office;
-        } else if (type === 'bankomat') {
-            pinOptions = mapPinImages.bankomat;
-        } else if (type === 'bankomat-partner') {
-            pinOptions = mapPinImages.bankomatPartners;
-        } else if (type === 'terminal') {
-            pinOptions = mapPinImages.terminal;
-        }
-        var placemark = new ymaps.Placemark([lat, lng], {}, pinOptions);
+        var placemark = new ymaps.Placemark(
+            [lat, lng],
+            {},
+            {
+                ...mapPinImages[type]
+            }
+        );
 
         innerMapInstance.geoObjects.add(placemark);
+    });
+
+    const suggestView = new ymaps.SuggestView('suggest', { results: 7 });
+
+    map.geoObjects.events.add('click', function(e) {
+        const object = e.get('target');
+
+        // console.log('Кликнутый геообъект', object);
+    });
+
+    suggestView.events.add('select', function(e) {
+        const suggestion = e.get('item');
+        ymaps.geocode(suggestion.value).then(
+            function(res) {
+                const firstGeoObject = res.geoObjects.get(0);
+                const coords = firstGeoObject.geometry.getCoordinates();
+
+                map.setCenter(coords, 12);
+            },
+            function(err) {
+                console.error(err);
+            }
+        );
     });
 }
