525523432ba85ad5c1dbf8e0e4e629e7d89c796d
Artem Safin <artemsafin.webdev@gmail.com>
2020-11-27 14:12:57 +0300

    Фиксы

--- src/js/bankOffices.js
+++ src/js/bankOffices.js
@@ -1,4 +1,5 @@
 import accordions from './accordions';
+import gsap from 'gsap';
 
 export default function() {
     const bankOffices = Array.from(document.querySelectorAll('.js-bank-offices'));
@@ -24,9 +25,78 @@ export default function() {
         setActiveLayer();
     });
 
+
+
+
+
+
+
+
+
+
+
+    document.addEventListener('click', event => {
+        if (event.target.matches('.js-accordion-btn') || event.target.closest('.js-accordion-btn')) {
+         
+            const accordion = event.target.closest('.js-offices-accordion');
+
+            if (!accordion) {
+                // console.error('No root accordion found');
+                return;
+            }
+            event.preventDefault();
+            
+
+
+            const content = accordion.querySelector('.offices__list-view-accordion-body');
+
+            if (accordion.classList.contains('active')) {
+                accordion.classList.remove('active');
+                gsap.to(content, {
+                    height: 0,
+                    duration: 0.4
+                })
+            } else {
+                accordion.classList.add('active');
+                gsap.to(content, {
+                    height: 'auto',
+                    duration: 0.4
+                })
+            }
+
+
+            const otherAccordions = Array.from(document.querySelectorAll('.js-offices-accordion'));
+
+            otherAccordions.forEach(otherAccordion => {
+                if (otherAccordion !== accordion && accordion.classList.contains('active')) {
+                    otherAccordion.classList.remove('active');
+                    const content = otherAccordion.querySelector('.offices__list-view-accordion-body')
+                    gsap.to(content, {
+                        height: 0,
+                        duration: 0.4
+                    })
+                }
+            })
+
+           
+        }
+    })
+
+
+
+
+
+
+
+
+
+
+    return;
+
     let officesAccordions = accordions(Array.from(document.querySelectorAll('.js-offices-accordion')));
 
     officesAccordions.init();
+    
 
     var target = document.getElementById('map-list');
 
